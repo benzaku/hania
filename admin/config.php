e412fbc7e8f9302ec0a260c9d58162aed2b7eeda
Haiyang Xu <haiyang.xu@me.com>
2017-12-10 11:28:51 +0100

    first commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://172.104.238.179/admin/');
+define('HTTP_CATALOG', 'http://172.104.238.179/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://172.104.238.179/admin/');
+define('HTTPS_CATALOG', 'http://172.104.238.179/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/weddingdress.com/public_html/admin/');
+define('DIR_SYSTEM', '/var/www/html/weddingdress.com/public_html/system/');
+define('DIR_IMAGE', '/var/www/html/weddingdress.com/public_html/image/');
+define('DIR_LANGUAGE', '/var/www/html/weddingdress.com/public_html/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/weddingdress.com/public_html/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/weddingdress.com/public_html/system/config/');
+define('DIR_CACHE', '/var/www/html/weddingdress.com/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/weddingdress.com/public_html/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/weddingdress.com/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/weddingdress.com/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/weddingdress.com/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/html/weddingdress.com/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'weddingdress');
+define('DB_PASSWORD', 'weddingdress');
+define('DB_DATABASE', 'weddingdress');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://172.104.238.179/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://172.104.238.179/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/weddingdress.com/public_html/catalog/');
+define('DIR_SYSTEM', '/var/www/html/weddingdress.com/public_html/system/');
+define('DIR_IMAGE', '/var/www/html/weddingdress.com/public_html/image/');
+define('DIR_LANGUAGE', '/var/www/html/weddingdress.com/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/weddingdress.com/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/weddingdress.com/public_html/system/config/');
+define('DIR_CACHE', '/var/www/html/weddingdress.com/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/weddingdress.com/public_html/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/weddingdress.com/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/weddingdress.com/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/weddingdress.com/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'weddingdress');
+define('DB_PASSWORD', 'weddingdress');
+define('DB_DATABASE', 'weddingdress');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
